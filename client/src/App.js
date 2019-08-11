557c1026c4ec4127bb8c156eedf179c11def5594
IamMonkie <monkie_225@yahoo.com>
2019-08-11 16:02:31 -0400

    initial commit, react project setup, unnecessary files removed, basic folder structure established

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>App.js test success</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
